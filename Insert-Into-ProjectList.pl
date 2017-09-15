f7a4a0f4a69daf46da32c0dae02219a3c4c4f749
Aingty Eung <aingtyeung@yahoo.com>
2017-09-14 21:12:19 -0700

    Checking Project number assignment

--- Insert-Into-ProjectList.pl
+++ Insert-Into-ProjectList.pl
@@ -8,52 +8,43 @@ $input = "\n     Usage\:  Insert-Into-ProjectList.pl [projectNumber]\n\n";
 $projectNumber = @ARGV[0] or die "$input";
 
 
-
-########## Please Update the following variables to suit your Project #########
-$projectNumber = 8020;
-$codeName = "'BCHE'";
-$databaseServer = "'ProjectList'";
-$server = "'Folding1'";
-$numberOfRun = 100;
-$numberOfClone = 100;
-$numberOfAtoms = 100;
-$description = "'This is the Project 8020 of BCHE.'";
-########### END of Variables to suit your Project ##############################
+print($projectNumber);
 
 
-############ DO NOT MAKE CHANGES UNLESS YOU KNOW WHAT TO DO ####################
-# Connecting to the Database Server Hosted by Banana
-$dbserver = "134.139.52.4:3306";
-my $dbh = DBI->connect("DBI:mysql:mysql:$dbserver",server,"") or print STDERR "Can't connect to mysql database on $dbserver\nTry giving this server permissions\n";
-print "Database connection established\n";
 
-# Once Connected, Insert Specific Project into the ProjectList
-$statement = $dbh->prepare("USE ProjectList");
-$statement->execute() or die "Could not use ProjectList Database: " . $statement->errstr();
-$statement = $dbh->prepare("INSERT INTO ProjectList
-                                (
-                                    projNum,
-                                    codeName,
-                                    dbServer,
-                                    server,
-                                    temperature,
-                                    numRun,
-                                    numClone,
-                                    numAtoms,
-                                    description
-                                )
-                                VALUES
-                                    (
-                                        $projectNumber,
-                                        $codeName,
-                                        $databaseServer,
-                                        $server,
-                                        $temperature,
-                                        $numberOfRun,
-                                        $numberOfClone,
-                                        $numberOfAtoms,
-                                        $description
-                                    )"
-                            );
-$statement->execute() or die "Could not insert Project $projectNumber into Database: " . $statement->errstr();
-print "Inserted the new Project into the Database.\n\n";
+# ############ DO NOT MAKE CHANGES UNLESS YOU KNOW WHAT TO DO ####################
+# # Connecting to the Database Server Hosted by Banana
+# $dbserver = "134.139.52.4:3306";
+# my $dbh = DBI->connect("DBI:mysql:mysql:$dbserver",server,"") or print STDERR "Can't connect to mysql database on $dbserver\nTry giving this server permissions\n";
+# print "Database connection established\n";
+
+# # Once Connected, Insert Specific Project into the ProjectList
+# $statement = $dbh->prepare("USE ProjectList");
+# $statement->execute() or die "Could not use ProjectList Database: " . $statement->errstr();
+# $statement = $dbh->prepare("INSERT INTO ProjectList
+#                                 (
+#                                     projNum,
+#                                     codeName,
+#                                     dbServer,
+#                                     server,
+#                                     temperature,
+#                                     numRun,
+#                                     numClone,
+#                                     numAtoms,
+#                                     description
+#                                 )
+#                                 VALUES
+#                                     (
+#                                         $projectNumber,
+#                                         $codeName,
+#                                         $databaseServer,
+#                                         $server,
+#                                         $temperature,
+#                                         $numberOfRun,
+#                                         $numberOfClone,
+#                                         $numberOfAtoms,
+#                                         $description
+#                                     )"
+#                             );
+# $statement->execute() or die "Could not insert Project $projectNumber into Database: " . $statement->errstr();
+# print "Inserted the new Project into the Database.\n\n";
