741610f3c31be7752f64e3b3e27e1b3c3529ee84
Xavier Martinez <xavier.martinez@student.csulb.edu>
2017-11-08 13:10:19 -0800

    Settled merge conflicts

--- config.py
+++ config.py
@@ -4,12 +4,18 @@
 SCOUT_CONFIGURATION = {
     'projects': {
         # Project Set ("Codename")
-        'Test': {
+        'BCHE': {
             # Project Directories
             'directories': {
                 # Boolean to determine if a Project is scoutable
                 # If true, scout the project for work
-                '/.../.../.../PROJ<#>': True
+                '/home/server/server2/data/SVR2257269762/PROJ8200': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8202': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8204': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8206': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8207': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8208': True,
+                '/home/server/server2/data/SVR2257269762/PROJ8209': True
             },
             # Boolean to determine if a Project Set is scoutable
             # If true, iterate over 'directories'
@@ -18,16 +24,16 @@ SCOUT_CONFIGURATION = {
     },
     # Path to lock file
     # If lock file exists do not execute scout
-    'lock': '/.../.../.../lock.txt',
+    'lock': '/home/server/server2/analysis/lock.txt',
     # Path to queue
-    'queue': '/.../.../.../queue.txt',
+    'queue': '/home/server/server2/analysis/queue.txt',
     # Path to work completed log
     # If a to-be-analyzed frame exists in work_completed do not queue
-    'work_completed': '/.../.../.../done.txt',
+    'work_completed': '/home/server/server2/analysis/done.txt',
     # Path to file that logs events that occur during normal operation
-    'log' : '/.../.../.../scout.log',
+    'log' : '/home/server/server2/analysis/scout-logs/scout.log',
     # Path to file that logs errors regarding a particular runtime event
-    'error_log' : '/.../.../.../scout-error.log'
+    'error_log' : '/home/server/server2/analysis/scout-logs/scout-error.log',
     # Slack webhook for the scout to POST error messages
     'webhook' : 'https://<url>.<goes>.<here>/...'
 }
