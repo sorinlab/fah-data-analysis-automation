7df88abe55f17c620402dd0d993f9bfebe93aacd
Xavier Martinez <xavier.martinez@student.csulb.edu>
2017-11-17 11:55:34 -0800

    Enhancements to F@H server error reporting bot

--- fah_server_error_reporting_bot.py
+++ fah_server_error_reporting_bot.py
@@ -2,8 +2,11 @@
 #
 # Follow the F@H log.txt file like tail -f.
 
+import datetime
+import subprocess
+import sys
 import time
-
+from error_reporting_bot import post_message
 
 def follow(thefile):
     thefile.seek(0, 2)
@@ -19,4 +22,10 @@ if __name__ == '__main__':
     logfile = open("/home/server/server2/log.txt", "r")
     loglines = follow(logfile)
     for line in loglines:
-        print line,
+        if 'Error from accept() call' in line:
+            current_time = str(datetime.datetime.now()).replace(' ', '-')
+            netstat_out = subprocess.check_output(['netstat', '-a'])
+            with open('/home/server/server2/{}.netstat.out'.format(current_time), 'w') as netstat_file:
+                netstat_file.write(netstat_out)
+            post_message('[ERROR] Folding1 WS process maxed out file descriptors! Send /home/server/server2/{current time}.netstat.out to the CSULB IT team!')
+            sys.exit()
