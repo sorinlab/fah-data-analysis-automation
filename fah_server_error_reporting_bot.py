a45dc3c69e940ec0152485c59d862397f2269e02
Xavier Martinez <xavier.martinez@student.csulb.edu>
2017-11-19 23:55:55 -0800

    enhancements

--- fah_server_error_reporting_bot.py
+++ fah_server_error_reporting_bot.py
@@ -27,5 +27,5 @@ if __name__ == '__main__':
             netstat_out = subprocess.check_output(['netstat', '-a'])
             with open('/home/server/server2/{}.netstat.out'.format(current_time), 'w') as netstat_file:
                 netstat_file.write(netstat_out)
-            post_message('[ERROR] Folding1 WS process maxed out file descriptors! Send /home/server/server2/{current time}.netstat.out to the CSULB IT team!')
+            post_message('[ERROR] Folding1 WS process maxed out file descriptors! Send /home/server/server2/{}.netstat.out to the CSULB IT team!'.format(current_time))
             sys.exit()
