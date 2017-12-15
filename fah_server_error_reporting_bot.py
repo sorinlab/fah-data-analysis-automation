95afb494a3f39e1f061dfcafd9b600857869b4bd
server <server@dhcp127-123.campus.ad.csulb.edu>
2017-12-15 14:18:25 -0800

    Updates error check/post message

--- fah_server_error_reporting_bot.py
+++ fah_server_error_reporting_bot.py
@@ -22,10 +22,7 @@ if __name__ == '__main__':
     logfile = open("/home/server/server2/log.txt", "r")
     loglines = follow(logfile)
     for line in loglines:
-        if 'Error from accept() call' in line:
-            current_time = str(datetime.datetime.now()).replace(' ', '-')
-            netstat_out = subprocess.check_output(['netstat', '-an'])
-            with open('/home/server/server2/{}.netstat.out'.format(current_time), 'w') as netstat_file:
-                netstat_file.write(netstat_out)
-            post_message('[ERROR] Folding1 WS process maxed out file descriptors! Send /home/server/server2/{}.netstat.out to the CSULB IT team!'.format(current_time))
+        if 'shutdown' in line:
+            current_time = str(datetime.datetime.now())
+            post_message('[ERROR] Folding2 WS shutdown!'.format(current_time))
             sys.exit()
